c8e68ea3644c13f0c620c881e481f580dd492066
JukioNT <jjulio2468@gmail.com>
2025-07-28 16:31:13 -0300

    Login with HASH complete

--- setCookie.js
+++ setCookie.js
@@ -1,8 +1,15 @@
 import { getCookie } from "./getCookie.js";
 
-async function verifyPassword(password) {
-	//Gambiarra mas funciona
+async function gerarHashCliente(senha, email) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(senha + email);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
 
+async function verifyPassword(password) {
 	const apiUrl = 'https://script.google.com/macros/s/AKfycbym0GgNgHq5HvQCoZJMY5jgS-AWs14tJj2VAnvDegK3EHTekPhqPU9cnLsRz4W9zMnn/exec';
 
 	const payload = {
@@ -44,12 +51,16 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
 	event.preventDefault();
 
 	const form = event.target;
+	const email = document.getElementById('user').value;
 	const password = document.getElementById('password').value;
 
-	if (password) {
-		const passwordIsCorrect = await verifyPassword(password);
+	if (password && email) {
+
+		const hashPassword = await gerarHashCliente(password, email);
+		
+		const passwordIsCorrect = await verifyPassword(hashPassword);
 		if(passwordIsCorrect){
-			document.cookie = `password=${encodeURIComponent(password)}; path=/; max-age=86400`;
+			document.cookie = `password=${encodeURIComponent(hashPassword)}; path=/; max-age=86400`;
 			window.location.href = "index.html";
 		}else{
 			console.log("Senha incorreta.");
